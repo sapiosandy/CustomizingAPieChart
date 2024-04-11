852c2065577dd3a89641ceb6b8f1e30284562163
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 12:12:16 -0700

    Initial Commit

---
+++ CustomizingAPieChart/CustomizingAPieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomizingAPieChartApp.swift
+//  CustomizingAPieChart
+//
+//  Created by Sandra Gomez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomizingAPieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
