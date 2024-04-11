104b2ea08b9ec73d80107ec9967ba389ca4a3530
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 12:20:07 -0700

    Customizing a pie chart (UCSD Extension)

--- CustomizingAPieChart/ContentView.swift
+++ CustomizingAPieChart/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var sales: Int
+    var id: String {name}
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", sales: 50),
+    .init(name: "Meyer", sales: 43),
+    .init(name: "George", sales: 62),
+    .init(name: "Hormel", sales: 26)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Chart(mySalesArray) { z in
+            SectorMark(
+                angle: .value("Sales", z.sales),
+                //adds space between angles
+                angularInset: 3.0
+            )
+            .foregroundStyle(by: .value("Name", z.name))
+            // dispalys numbers / values on the angles
+            .annotation(position: .overlay) {
+                Text ("\(z.sales)")
+                    .foregroundStyle(.white)
+            }
+        } .padding()
     }
 }
 
